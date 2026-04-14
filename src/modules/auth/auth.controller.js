81ee481cde1d22fc19078872b520dfa779033282
carolross1 <carolrios347@gmail.com>
2026-04-13 22:47:30 -0600

    fix: cajero, mis pedidos, recuperar contraseña

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -104,7 +104,8 @@ const login = async (req, res) => {
         id: user.id, 
         email: user.email, 
         rol: user.rol,
-        nombre: user.nombre 
+        nombre: user.nombre, 
+         iMesaId: user.mesa ? user.mesa.id : null
       },
       process.env.JWT_SECRET,
       { expiresIn: '24h' }
